5f87c2c9deca9f366f333cb4d884adaf57b8d559
hiehoox7 <7wdhh2+1c7a5l2gjrqcwmy2chxdv2wf@sharklasers.com>
2017-06-02 21:19:56 +0400

    COMPLETE FORK
    change a whole LOT, i wanted to start a seperate respositorty for this.
    *added getting router channel
    *displaying more info
    *ascii art
    *colors
    *graceful exit
    *install missing packages
    *other stuff i don't rememeber
    *probably moe

--- shARP.sh
+++ shARP.sh
@@ -1,202 +1,263 @@
 #! /bin/bash 
+# Forked by:
+#▂▃▅▇█▓▒░ HIEHOOX7 ░▒▓█▇▅▃▂ 
+#
+# USAGE :
+# sudo ./smARP.sh -[o;d;r] <iface>
+# -o or --offense ==> attacks the arp spoofer once detected ( kicks them off the network )
+# -d or --deffense ==> defences you from the arp spoofer
+# -r or --reset ==> resets the interace state
+# <iface> ==> network interface ( e.g wlan0 )
+
+
+# This script needs root permissions to work
+echo "Please provide root priveleges"
+sudo clear
+
+#############
+# EXIT TRAP #
+#############
+
+#CTRL+C trap -- when you press CTRL+C the ctrl_c() function will be executed 
+#this function does a 'graceful' exit of the script
+trap ctrl_c INT
+ctrl_c () {
+	echo -e "\n${BOLD}--- CLEANING UP ---\n"
+	echo -e "\n[${YELLOW}*${NC}] Stopping monitor mode.."
+	sudo airmon-ng stop ${iface_mon}
+	sudo ifconfig ${iface} down
+	echo -e "\n[${YELLOW}*${NC}] Resetting mac.."
+	sudo macchanger ${iface} -a
+	sudo ifconfig ${iface} up
+	echo -e "[${YELLOW}*${NC}] Restarting Network Manager.."
+	sudo service network-manager restart
+	clear
+	echo -e "[${YELLOW}*${NC}] Exitting in 3..."
+	sleep 1
+	clear
+	echo -e "[${YELLOW}*${NC}] Exitting in 2.."
+	sleep 1
+	clear
+	echo -e "[${YELLOW}*${NC}] Exitting in 1."
+	sleep 1
+	sudo nmcli radio wifi on
+cat << "EOF"
+
+##########################
+# THANKS FOR USING smARP #
+###################################
+# the credit for the script goes ##
+# to the original creator of shARP#
+# I just went ahead  ##############
+# and forked it because ###### 
+# i didn't like it  ##### 
+#######################
+# BY #
+############################
+#▂▃▅▇█▓▒░ HIEHOOX7 ░▒▓█▇▅▃▂ #
+############################
+EOF
+echo -e ${YELLOW}
+cat << "EOF"
+              _
+             ( )
+             | |      _
+             | |_ _  / |
+         __  |-/ \ \/ /
+        /_ `\|_| | | /
+          `\ ` \_/_/-|
+            \    '   |
+             \  .`  /____________________
+             |     |/                    \
+            /     / | *Keep it cool brah |
+            |     | \_   ________________/
+             \     \  |/
+              | .__.\
+              \_____/ 
+              
+EOF
+echo -e ${NC}
+exit
+}
 
 
 
-echo "				           ||                               _______       _______       "     
-echo "				           ||                  /\          |        ?    |        ?     "
-echo "				           ||                 /  \         |         ?   |         ?    "
-echo "				           ||                /    \        |         ?   |         ?    "
-echo "				     //    ||-------        /      \       |________?    |________?     "
-echo "				    //     ||      ||      /--------\      |     \       |              "
-echo "				   //_____ ||      ||     /          \     |      \      |              "
-echo "				        // ||      ||    /            \    |       \     |             "
-echo "				       //  ||      ||   /              \   |        \    |             "
-echo "				      //   ||      ||  /                \  |         \   |             "
-echo "	__________________________________________________________________________________________________________________	  		    				       		                       "
- 
+#######################
+# DECLARING VARIABLES #
+#######################
+
+# Defining colors
+RED='\033[1;31m'
+NB_RED='\033[0;31m' # NOT BOLD RED
+GREEN='\033[1;32m'
+BLUE='\033[1;34m'
+PURPLE='\033[1;35m'
+CYAN='\033[1;36m'
+LIGHTCYAN='\033[1;36m'
+YELLOW='\033[1;33m'
+BOLD='\033[1m'
+NC='\033[0m' #No Color
+
+#iface_mon=""
+
+#default gateway ( e.g 192.168.1.1 )
+ROUTER_GATEWAY=$(ip route list | sed -n -e "s/^default.*[[:space:]]\([[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\).*/\1/p")
+
+#checking if the network interface is passed to the bash script, if not then ask for it
+if [ -z $2 ]; then
+   echo -n -e "[${YELLOW}?${NC}] Network Interface for script (i.e ${GREEN}wlan0${NC}):${GREEN} "
+   read iface
+   iface_mon=${iface}mon
+   echo -e "${NC}"
+else
+   iface=$2   
+   iface_mon=$2'mon'    # e.g wlan0mon
+fi
 
 
+#checking if the network interface is passed to the bash script, if not then ask for it
+if [ -z $1 ]; then
+   echo -n -e "[${YELLOW}?${NC}] Choose script mode (offense,defense,reset) : [${YELLOW}o;d;r${NC}] : "
+   read script_arg
+   script_arg='-'${script_arg}
+   echo -e "${NC}"
+else
+   script_arg=$1   
+fi
 
-arg=$1
-interface=$2
-if [ " $arg " = ' -r ' ] || [ " $arg " = ' --reset ' ]; then
-interfacemon=$interface'mon'
-airmon-ng stop $interfacemon ; 
-ifconfig $interface down ; 
-ifconfig $interface up ;
-service network-manager restart ; 
-sleep 10
-#==================================================================================================================================================
-else if
- [ " $arg " = ' -d ' ] || [ " $arg " = ' --defence ' ]; then
- gtping=`ping gateway -c 1`
- gtping=${gtping%gateway*} #retain the part before gateway
- gtping=${gtping%gateway*} #retain the part before gateway
- gtping=${gtping%gateway*} #retain the part before gateway
-  
- if [ " $gtping " = " PING  " ] ; then 
-  MYVAR=`sudo arp gateway -i $interface `
-  add=${MYVAR%C*}  # retain the part after the C
-  add=${add##*r}  # retain the part before the ether
-  if [  /usr/shARP/ = false ] && [/usr/shARP/ != true ]; then
-    sudo mkdir /usr/shARP 
-  fi 
-  echo  $(date +"%D") ": DEFENSIVE MODE : The original address of the gateway is " $add >> /usr/shARP/gateway.txt
-  arp gateway -i $interface
-  arpout=`arp gateway -i $interface`
-  echo ............................................................................................................................................
-  arpd=$add
-  carpd=$arpd
-  carpd=${carpd//[ ]/}
-  vendor=${carpd//[:]/}
-  vendor=${vendor:0:6}
-  while read line
-                                       do macid=${line:0:6}
-					   vendor="${vendor^^}"
-                                       if [ " $macid " = " $vendor " ]
-				        then 
-                                         echo "Your current gateway is " $add " and your MAC Vendor is " ${line:(+6)}
-                                          
-                                       fi
-				       
-                                       done < mac-vendors.txt
+# Determining the router channel from wireless interface frequency using iwconfig
+router_channel=`sudo iwconfig ${iface} | awk 'match($0,"Frequency"){print substr($0,RSTART+10,+5)}'`
+if [[ ${router_channel} == '2.412' ]] ; then
+	router_channel='1'
+elif [[ ${router_channel} == '2.417' ]] ; then
+	router_channel='2'
+elif [[ ${router_channel} == '2.422' ]] ; then
+	router_channel='3'
+elif [[ ${router_channel} == '2.427' ]] ; then
+	router_channel='4'
+elif [[ ${router_channel} == '2.432' ]] ; then
+	router_channel='5'
+elif [[ ${router_channel} == '2.437' ]] ; then
+	router_channel='6'
+elif [[ ${router_channel} == '2.442' ]] ; then
+	router_channel='7'
+elif [[ ${router_channel} == '2.447' ]] ; then
+	router_channel='8'
+elif [[ ${router_channel} == '2.452' ]] ; then
+	router_channel='9'
+elif [[ ${router_channel} == '2.457' ]] ; then
+	router_channel='10'
+elif [[ ${router_channel} == '2.462' ]] ; then
+	router_channel='11'
+elif [[ ${router_channel} == '2.467' ]] ; then
+	router_channel='12'
+elif [[ ${router_channel} == '2.472' ]] ; then
+	router_channel='13'
+elif [[ ${router_channel} == '2.484' ]] ; then
+	router_channel='14'
+fi
 
-  echo ........................................................................................................................................... 
-	while true
-		do	
+###########################
+# FULFILLING REQUIREMENTS #
+###########################
 
-			output=`sudo arp gateway -i $interface`
-			carpd=${output%C*}  # delete the part after the C
-			carpd=${carpd##*r}  # delete the part before the ether
-			arpd=${arpd//[ ]/}
-			carpd=${carpd//[ ]/}
-			
-			if [ " $arpd " != " $carpd " ]; then
-				echo "gateway changed from " $arpd " to " $carpd " at time " $(date +"%T") 
-				echo $carpd "is spoofing!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! "
-  				echo "network connection going down "
-				ifconfig $interface down
-				
-                                vendor=${carpd//[:]/}
-                                vendor=${vendor:0:6}
-                                while read line
-                                       do macid=${line:0:6}
-					   vendor="${vendor^^}"
-                                       if [ " $macid " = " $vendor " ]
-				        then 
-                                         echo "MAC Vendor of the attacker is " ${line:(+6)}
-                                          macvendor=${line:(+6)}
-                                       fi
-				
-                                       done < mac-vendors.txt
-				echo "gateway changed from" $arpd " to" $carpd " at time " $(date +"%T") " on " $(date +"%D") "The attacker's MAC vendor is " $macvendor >> /usr/shARP/log.txt  
-				espeak  'network is being spoofed by '$carpd', connection, going down. Contact your network administrator.' 
-				exit         
-			fi ; done;  else echo "gateway not found"
-   fi
-#==================================================================================================================================================
+# checks whether the packages needed are installed or not , and acts acordinglly
+if [ $(dpkg-query -W -f='${Status}' macchanger 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+	echo -e "\n MACCHANGER not detected , installing \n"
+	sudo apt-get install macchanger
+fi
+if [ $(dpkg-query -W -f='${Status}' aircrack-ng 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+	echo -e "\n AIRCRACK-NG not detected , installing \n"
+	sudo apt-get install aircrack-ng
+fi
 
-else if [ " $arg " = ' -o ' ] || [ " $arg " = ' --offence ' ]; then 
-   gtping=`ping gateway -c 1`
-   gtping=${gtping%gateway*} #retain the part before gateway
-   gtping=${gtping%gateway*} #retain the part before gateway
-   gtping=${gtping%gateway*} #retain the part before gateway
-  
-   if [ " $gtping " = " PING  " ] ; then 
-    output=`sudo arp gateway -i $interface `
-    add=${output%C*}  # retain the part after the C
-    add=${add##*r}  # retain the part before the ether
+#Creating a directory for files
+if [  smARP-files/ = false ] && [ smARP-files/ != true ]; then
+sudo mkdir smARP-files  
+fi
 
-    if [  /usr/shARP/ = false ] && [/usr/shARP/ != true ]; then
-      mkdir /usr/shARP/
-    fi
 
-    echo $(date +"%D") " : OFFENSIVE MODE : The original address of the gateway is " $add >> /usr/shARP/gateway.txt
-    arp gateway -i $interface
-    echo .................................................................................................................................................
-    arpd=$add
-    carpd=$arpd
-    carpd=${carpd//[ ]/} 
-    vendor=${carpd//[:]/}
-    vendor=${vendor:0:6}
-    while read line
-                                       do macid=${line:0:6}
-					   vendor="${vendor^^}"
-                                       if [ " $macid " = " $vendor " ]
-				        then 
-                                         echo "Your current gateway is " $add " and your MAC Vendor is " ${line:(+6)}
-                                          
-                                       fi
-				       
-                                       done < mac-vendors.txt
-   echo ................................................................................................................................................. 
-   while true
-		do
-			
-			MYVAR=`sudo arp gateway -i $interface`
+##############
+# RESET MODE #
+##############
 
-			carpd=${MYVAR%C*}  # retain the part after the C
-			carpd=${carpd##*r}  # retain the part before the ether
-			arpd=${arpd//[ ]/}
-			carpd=${carpd//[ ]/}
-			
-			if [ " $arpd " != " $carpd " ]; then 
-				echo "gateway changed from " $arpd " to " $carpd " at time " $(date +"%T") 
-				echo $carpd "is spoofing!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! "
-  				echo "network connection going down "
-				
-				
-                                vendor=${carpd//[:]/}
-                                vendor=${vendor:0:6}
-                                while read line
-                                       do macid=${line:0:6}
-                                       if [ " $macid " = " $vendor " ]
-				        then 
-                                         echo "MAC Vendor of the attacker is " ${line:(+6)} 
-					macvendor=${line:(+6)}                                       
-					fi
-				       done < mac-vendors.txt
-                           
-                         echo "gateway changed from " $arpd " to " $carpd " at time " $(date +"%T") " on " $(date +"%D") "The attacker's MAC vendor is " $macvendor >> /usr/shARP/log.txt 
-			
-			hash aircrack-ng >> /usr/shARP/hash1.txt 
-			hash airmon-ng >> /usr/shARP/hash2.txt 
-			r1hash=$(cat /usr/shARP/hash1.txt)
-			r2hash=$(cat /usr/shARP/hash2.txt)
-			if [ " $r2hash " = "bash: hash: airmon-ng: not found" ] || [ " $r1hash " = "bash: hash: aircrack-ng: not found" ] ;then
-        	  
-			sudo apt-get install aircrack-ng
-        			
-			echo "airmon-ng and aircrack-ng are starting"
-		else 
-			echo "airmon-ng and aircrack-ng are starting"
-		fi
-			sudo airmon-ng start $interface
-			sudo airmon-ng check kill 
-			while true 
-			  do sudo aireplay-ng -0 1000 -a $arpd -c $carpd $interface'mon' 
-			done
-			exit
-		fi;done ; else echo "gateway not found" ; fi 
-   
- 	else if [ " $arg " = ' -h ' ] || [ " $arg " = ' --help ' ]; then
-		echo "bash ./shARP.sh --[option] [interface] "
-		echo "[option]"
-		echo ""
-		echo "-d OR --defence = defend your system from arp spoofing or man in the middle attacks"
-		echo ""
-		echo "-o OR --offence = remove the arp spoofer from the network :WARNING: network inteface would go down while removing the spoofer " 
-        	echo "	          from the network. aircrack-ng would get installed in your system if not present beforehand." 
-        	echo "                  An active internet connection would be required for this purpose "  
-		echo ""
-		echo "-r OR --reset   = reset network interface card and network manager (suggested to use after operating the offensive mode.)"
-		echo ""
-		echo "[interface]  = the network device you are currently using "
+if [ " $script_arg " = ' -r ' ] || [ " $script_arg " = ' --reset ' ]; then
+	ctrl_c
+fi
+
+
+################
+# DEFENCE MODE #
+################
+
+#ascii art
+cat ascii.art
+echo -e "forked by ${NB_RED} ▂▃▅▇█▓▒░ 31CHEECH ░▒▓█▇▅▃▂  ${NC}"
+
+if [ " $script_arg " = ' -d ' ] || [ " $script_arg " = ' --defence ' ]; then
+	echo -e "\n ${CYAN} defence mode  ${NC}"
+elif [ " $script_arg " = ' -o ' ] || [ " $script_arg " = ' --offense ' ]; then
+	echo -e "\n ${NB_RED} offense mode  ${NC}"
+fi
+
+router_mac_old=`sudo arp gateway -i wlan0 | grep gateway | awk '{print $3}'`
+iface_ipv4=`hostname -I`
+echo -e "\n INTERFACE IPV4 ADDRESS : ${GREEN} ${iface_ipv4} ${NC}"
+echo -e " ROUTER MAC ADDRESS : C${GREEN}${router_mac_old}${NC}"
+echo -e " ROUTER CHANNEL : ${GREEN}${router_channel}${NC}"
+echo -e "${YELLOW} scanning... ${NC}"
+#uncomment the next line if you want to turn on logging
+#sudo echo
+#echo $(date +"%D") ${router_mac} > smARP-files/gateway.txt #writing mac to logfile 
+
+while :
+do
+	router_mac_new=`sudo arp gateway -i ${iface}`
+	router_mac_new=${router_mac_new%C*}  # delete the part after the C
+	router_mac_new=${router_mac_new##*r}  # delete the part before the ether
+	router_mac_new=${router_mac_new//[ ]/}
+	router_mac=${router_mac_old//[ ]/}
 	
-	   else 
-		echo "use bash ./shARP.sh -[option] ........... use bash ./shARP.sh -h for help"
-   
-fi ;fi; fi;fi
-
-
-
+	
+	if [ " $router_mac_new " != " $router_mac " ]; then
+		echo -e " Gateway MAC changed from ${GREEN} ${router_mac} ${NC} to ${RED} ${router_mac_new} ${NC} at time : " $(date +"%T") 
+		echo -e "${RED} ${router_mac_new} ${NC} is likely ${BOLD} poisoning your arp cache ${NC}"
+		echo -e "Attacker MAC : ${router_mac_new} --- $(date +"| %D | %H:%M:%S")" > smARP-files/attack-log.txt
+		if [ " $script_arg " = ' -r ' ] || [ " $script_arg " = '--reset ' ] ; then
+			#exectues the same function as if you press CTRL+C , see "EXIT TRAP" at the start of the code
+			sudo ctrl_c
+			exit
+		elif [ " $script_arg " = '-d' ] || [ " $script_arg " = ' --defence ' ]; then
+			echo -e " ${BOLD}Executing defense procedure${NC}"
+			sudo airmon-ng check kill
+			ifconfig ${iface} down
+			service network-manager start
+			ifconfig ${iface} up
+			exit
+		elif [ " $script_arg " = ' -o ' ] || [ " $script_arg " = ' --offence ' ]; then
+			echo -e " ${BOLD}Executing attack procedure${NC}"
+			sudo airmon-ng check kill
+			sudo airmon-ng start ${iface}
+			sudo ifconfig ${iface_mon} down
+			sudo macchanger ${iface_mon} -a
+			echo -e "${BOLD}switching channel${NC}"
+			sudo iwconfig ${iface_mon} channel ${router_channel}
+			sudo ifconfig ${iface_mon} up
+			sleep 3
+			echo -e "${BOLD} TAKING OUT THE ${RED} HARPOONS${BOLD}!${NC}"
+			echo -e "${BOLD} deauthing attacker 1000 times"
+			sudo aireplay-ng --deauth 1000 -c ${router_mac_new} -a ${router_mac} ${iface_mon}
+			sleep 3
+			sudo airmon-ng stop ${iface}
+			sudo ifconfig ${iface_mon} up
+			sudo service network-manager start
+			sleep 5
+			sudo nmcli radio wifi on
+			sleep 5	
+			exit
+		fi
+		exit
+	fi
+done
